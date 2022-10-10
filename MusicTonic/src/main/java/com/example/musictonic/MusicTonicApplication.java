051b28bc2856e9aad3cc15ed4d41189e84f08f92
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-10-10 12:08:49 -0400

    basic structure

--- MusicTonic/src/main/java/com/example/musictonic/MusicTonicApplication.java
+++ MusicTonic/src/main/java/com/example/musictonic/MusicTonicApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @SpringBootApplication
 @RestController
 public class MusicTonicApplication {
