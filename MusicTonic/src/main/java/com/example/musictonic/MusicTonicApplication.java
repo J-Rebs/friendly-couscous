dc86baafce255e034aad0d64fdc33882cd85a008
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-10-09 20:40:48 -0400

    rest of files

---
+++ MusicTonic/src/main/java/com/example/musictonic/MusicTonicApplication.java
@@ -0,0 +1,14 @@
+package com.example.musictonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTonicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicTonicApplication.class, args);
+    }
+
+
+}
---
+++ MusicTonic/src/test/java/com/example/musictonic/MusicTonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musictonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTonicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
