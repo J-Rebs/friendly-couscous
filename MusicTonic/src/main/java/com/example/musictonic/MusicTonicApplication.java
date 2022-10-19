b522a0fd8b07cca7542069eb7e33cd948d7bcdcf
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-10-19 19:46:03 -0400

    CORS functionality added -- attempt 4

--- MusicTonic/src/main/java/com/example/musictonic/MusicTonicApplication.java
+++ MusicTonic/src/main/java/com/example/musictonic/MusicTonicApplication.java
@@ -2,7 +2,9 @@ package com.example.musictonic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -12,5 +14,13 @@ public class MusicTonicApplication {
         SpringApplication.run(MusicTonicApplication.class, args);
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://amazonaws.com");
+            }
+        };
+    }
 }
