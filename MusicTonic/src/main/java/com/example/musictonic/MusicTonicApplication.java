b607eec3dbeef7896ba67cb2dd9b41b3b2e9d713
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-10-19 01:04:15 -0400

    routing commits
    CORS functionality added
    
    CORS functionality added -- attempt 2
    
    CORS functionality added -- attempt 3
    
    CORS functionality added -- attempt 5
    
    CORS functionality added -- attempt 6
    
    updates to pom to help build properly for heroku (see: https://stackoverflow.com/questions/37400444/heroku-spring-boot-web-service-always-return-404-but-not-on-localhost)
    
    remove poms changes

--- MusicTonic/src/main/java/com/example/musictonic/MusicTonicApplication.java
+++ MusicTonic/src/main/java/com/example/musictonic/MusicTonicApplication.java
@@ -2,11 +2,9 @@ package com.example.musictonic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -16,5 +14,13 @@ public class MusicTonicApplication {
         SpringApplication.run(MusicTonicApplication.class, args);
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://music-tonic.herokuapp.com/","https://music-tonic.herokuapp.com/favicon.ico");
+            }
+        };
+    }
 }
