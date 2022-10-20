f6eddf1a29d299746d4648c3028b4c891f8a26d1
Ryan Soeyadi <rs4163@columbia.edu>
2022-10-19 23:11:11 -0400

    added playlist object model

---
+++ MusicTonic/src/main/java/com/example/musictonic/model/Playlist.java
@@ -0,0 +1,79 @@
+package com.example.musictonic.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "playlists")
+public class Playlist {
+
+    // definition of entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "serial")
+    private Long PlaylistId;
+
+    @Column(name = "owner")
+    private String owner;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "isDefault")
+    private Boolean isDefault;
+
+    // constructors
+    // add default constructor for sake of JPA
+    protected Playlist() {
+    }
+
+    public Playlist(Long playlistId, String owner, String name, Boolean isDefault) {
+        PlaylistId = playlistId;
+        this.owner = owner;
+        this.name = name;
+        this.isDefault = isDefault;
+    }
+
+    // getters and setters
+    public Long getPlaylistId() {
+        return PlaylistId;
+    }
+
+    public void setPlaylistId(Long playlistId) {
+        PlaylistId = playlistId;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getDefault() {
+        return isDefault;
+    }
+
+    public void setDefault(Boolean aDefault) {
+        isDefault = aDefault;
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist{" +
+                "PlaylistId=" + PlaylistId +
+                ", owner='" + owner + '\'' +
+                ", name='" + name + '\'' +
+                ", isDefault=" + isDefault +
+                '}';
+    }
+
+}
