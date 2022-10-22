b9df0580f095ac8c6ccf9147cea1c8527e8f2417
Ryan Soeyadi <rs4163@columbia.edu>
2022-10-21 23:51:33 -0400

    more sql statements

--- MusicTonic/src/main/resources/import.sql
+++ MusicTonic/src/main/resources/import.sql
@@ -1,5 +1,23 @@
+/* client generation */
 INSERT INTO clients (client_name) VALUES ('clientest');
-INSERT INTO users (age, main_genre, real_name, user_type) VALUES (27, 'pop', 'Joe', 1);
+INSERT INTO clients (client_name) VALUES ('second test client');
+
+/* user generation for LISTENERS */
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (27, 'pop', 'Joe', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (43, 'rap', 'Jae', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (28, 'country', 'John', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (15, 'pop', 'Arthur', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (25, 'classical', 'Joe', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (53, 'country', 'Josephina', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (20, 'metal', 'Megan', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (27, 'techno', 'Helen', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (46, 'country', 'Liag', 2);
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (40, 'classical', 'Alexandra', 2);
+
+/* playlist generation */
+INSERT INTO playlists (is_default, name, owner) VALUES (False, 'CS4LYFELoFI','Joe');
+INSERT INTO playlists (is_default, name, owner) VALUES (False, 'ASE Stress Reliever','Josephina');
+
 INSERT INTO playlist_to_subscriber (playlist_id, user_id) VALUES (1,1);
 INSERT INTO songs (song_artist, song_duration, song_likes_count, song_lyrics, song_name) VALUES ('Chief Keef', 150, 100000, 'love sosa', 'love sosa');
 INSERT INTO playlist_to_songs (playlist_id, song_id) VALUES (1, 1);
