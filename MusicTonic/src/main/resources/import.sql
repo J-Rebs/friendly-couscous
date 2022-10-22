d07779d9b08ac3720f49b35c2f630056c0e22a02
Ryan Soeyadi <rs4163@columbia.edu>
2022-10-21 23:38:06 -0400

    more sql statements

--- MusicTonic/src/main/resources/import.sql
+++ MusicTonic/src/main/resources/import.sql
@@ -1,2 +1,5 @@
-insert into clients (client_name) values ('clientest');
-insert into users (age, main_genre, real_name, user_type) VALUES (27, 'pop', 'Joe', 1);
+INSERT INTO clients (client_name) VALUES ('clientest');
+INSERT INTO users (age, main_genre, real_name, user_type) VALUES (27, 'pop', 'Joe', 1);
+INSERT INTO playlist_to_subscriber (playlist_id, user_id) VALUES (1,1);
+INSERT INTO songs (song_artist, song_duration, song_likes_count, song_lyrics, song_name) VALUES ('Chief Keef', 150, 100000, 'love sosa', 'love sosa')
+INSERT INTO playlist_to_songs (playlist_id, song_id) VALUES (1, 1)
