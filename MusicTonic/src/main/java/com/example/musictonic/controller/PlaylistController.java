0dc683d94309bd7a5a5f5b76f1eca33a005b1351
Ryan Soeyadi <rs4163@columbia.edu>
2022-10-24 11:48:22 -0400

    prelim testing + removed unused controllers

--- MusicTonic/src/main/java/com/example/musictonic/controller/PlaylistController.java
+++
@@ -1,9 +0,0 @@
-package com.example.musictonic.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PlaylistController {
-
-
-}
---
+++ MusicTonic/src/test/java/com/example/musictonic/controller/Client1ControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.musictonic.controller;
+
+import com.example.musictonic.services.Client1Service;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/* SRC: https://stackoverflow.com/questions/44200720/difference-between-mock-mockbean-and-mockito-mock */
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(Client1Controller.class)
+class Client1ControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    Client1Service client1Service;
+
+    @Test
+    void playSong() throws Exception {
+        mvc.perform(get("/client1-rest/playsong"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void createAnalyticalSongs() {
+    }
+}
