72d520422be59be854e2412037adfba33a320556
Ryan Soeyadi <rs4163@columbia.edu>
2022-10-24 21:30:44 -0400

    wrote two basic unit tests--one tests if bad request to playsong route

--- MusicTonic/src/test/java/com/example/musictonic/controller/Client1ControllerTest.java
+++ MusicTonic/src/test/java/com/example/musictonic/controller/Client1ControllerTest.java
@@ -1,38 +1,22 @@
 package com.example.musictonic.controller;
 
-import com.example.musictonic.config.TestProfileConfig;
-import com.example.musictonic.model.Playlist;
-import com.example.musictonic.model.Song;
-import com.example.musictonic.model.User;
-import com.example.musictonic.model.UserType;
-import com.example.musictonic.repository.PlaylistRepository;
-import com.example.musictonic.repository.SongRepository;
 import com.example.musictonic.repository.UserRepository;
 import com.example.musictonic.services.Client1Service;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-
-/* SRC: https://stackoverflow.com/questions/44200720/difference-between-mock-mockbean-and-mockito-mock */
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(Client1Controller.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = Client1Controller.class)
 class Client1ControllerTest {
 
     @Autowired
@@ -42,33 +26,40 @@ class Client1ControllerTest {
     private UserRepository userRepo;
 
     @MockBean
-    private PlaylistRepository playlistRepo;
-
-    @MockBean
     private Client1Service client1Service;
 
-    @MockBean
-    private SongRepository songRepo;
-
     @Test
-    void validPlaySong() throws Exception {
-        User user = new User(1L, "Cool Guy", UserType.ARTIST, "country", 27);
-        Song song = new Song(1L, "SongySongyPopPop", 2, "CoolestArtist", "YaYaMerchMakesYou COOL", 10);
-        Playlist playlist = new Playlist(1L, "CoolGuy", "TheBEST", false);
-        userRepo.save(user);
-        songRepo.save(song);
-        playlistRepo.save(playlist);
-
-        MvcResult res = mvc.perform(get("/client1-rest/playsong"))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        String content = res.getResponse().getContentAsString();
-        System.out.print("RESULT OF GET VALID PLAYSONG" + content);
+    void playSongTest() throws Exception {
+        this.mvc.perform(get("/client1-rest/playsong"))
+                .andExpect(status().isOk());
     }
 
-
-
-
+    @Test
+    void createAnalyticalSongBad() throws Exception {
+        mvc.perform(post("/client1-rest/playsong?userid=1&songid=1&playlistid=1"))
+                .andExpect(status().isBadRequest());
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
