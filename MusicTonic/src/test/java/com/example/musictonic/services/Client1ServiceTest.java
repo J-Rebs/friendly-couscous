c44866adcf7311788288a0c1a36c0493a836ef3e
Ryan Soeyadi <rs4163@columbia.edu>
2022-10-28 21:57:33 -0400

    likeSongGood() unit test

--- MusicTonic/src/test/java/com/example/musictonic/services/Client1ServiceTest.java
+++ MusicTonic/src/test/java/com/example/musictonic/services/Client1ServiceTest.java
@@ -1,8 +1,7 @@
 package com.example.musictonic.services;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -14,15 +13,11 @@ import com.example.musictonic.model.Playlist;
 import com.example.musictonic.model.Song;
 import com.example.musictonic.model.User;
 import com.example.musictonic.model.UserType;
-import com.example.musictonic.repository.AnalyticsPlaylistRepository;
-import com.example.musictonic.repository.AnalyticsRepository;
-import com.example.musictonic.repository.AnalyticsSongRepository;
-import com.example.musictonic.repository.AnalyticsUserRepository;
-import com.example.musictonic.repository.PlaylistRepository;
-import com.example.musictonic.repository.SongRepository;
-import com.example.musictonic.repository.UserRepository;
+import com.example.musictonic.repository.*;
+
 import java.sql.Timestamp;
 import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,90 +26,110 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static java.lang.Integer.valueOf;
+
 @ExtendWith(MockitoExtension.class)
 class Client1ServiceTest {
 
-  @Mock
-  AnalyticsRepository analyticsRepo;
+    @Mock
+    AnalyticsRepository analyticsRepo;
 
-  @Mock
-  AnalyticsUserRepository analyticsUserRepo;
+    @Mock
+    AnalyticsUserRepository analyticsUserRepo;
 
-  @Mock
-  AnalyticsSongRepository analyticsSongRepo;
+    @Mock
+    AnalyticsSongRepository analyticsSongRepo;
 
-  @Mock
-  AnalyticsPlaylistRepository analyticsPlaylistRepo;
+    @Mock
+    AnalyticsPlaylistRepository analyticsPlaylistRepo;
 
-  @Mock
-  UserRepository userRepo;
+    @Mock
+    UserRepository userRepo;
 
-  @Mock
-  SongRepository songRepo;
+    @Mock
+    SongRepository songRepo;
 
-  @Mock
-  PlaylistRepository playlistRepo;
+    @Mock
+    PlaylistRepository playlistRepo;
 
-  @Mock
-  AnalyticsUserRepository analyticsUserRepository;
+    @Mock
+    AnalyticsUserRepository analyticsUserRepository;
 
-  @Mock
-  AnalyticsSongRepository analyticsSongRepository;
+    @Mock
+    AnalyticsSongRepository analyticsSongRepository;
 
-  @Mock
-  AnalyticsPlaylistRepository analyticsPlaylistRepository;
+    @Mock
+    AnalyticsPlaylistRepository analyticsPlaylistRepository;
 
-  @InjectMocks
-  Client1Service client1Service;
+    @Mock
+    PlaylistToSongRepository playlistToSongRepository;
 
-  private User user;
-  private Song song;
-  private Playlist playlist;
-  private Analytics a;
+    @InjectMocks
+    Client1Service client1Service;
 
-  private AnalyticsUser analyticsUser;
-  private AnalyticsSong analyticsSong;
-  private AnalyticsPlaylist analyticsPlaylist;
+    private User user;
+    private Song song;
+    private Playlist playlist;
+    private Analytics a;
 
-  private Timestamp timestamp;
+    private AnalyticsUser analyticsUser;
+    private AnalyticsSong analyticsSong;
+    private AnalyticsPlaylist analyticsPlaylist;
 
-  @BeforeEach
-  void init() {
-    user = new User(1L, "Cool Guy", UserType.ARTIST, "country", 27);
-    song = new Song(1L, "SongySongyPopPop", 2, "CoolestArtist", "YaYaMerchMakesYou COOL", 10);
-    playlist = new Playlist(1L, 1L, "TheBEST", false);
+    private Timestamp timestamp;
 
-    analyticsUser = new AnalyticsUser(a, user);
-    analyticsSong = new AnalyticsSong(a, song);
-    analyticsPlaylist = new AnalyticsPlaylist(a, playlist);
+    @BeforeEach
+    void init() {
+        user = new User(1L, "Cool Guy", UserType.ARTIST, "country", 27);
+        song = new Song(1L, "SongySongyPopPop", 2, "CoolestArtist", "YaYaMerchMakesYou COOL", 10);
+        playlist = new Playlist(1L, 1L, "TheBEST", false);
 
-    Date date = new Date();
-    timestamp = new Timestamp(date.getTime());
-    a = new Analytics(timestamp);
+        analyticsUser = new AnalyticsUser(a, user);
+        analyticsSong = new AnalyticsSong(a, song);
+        analyticsPlaylist = new AnalyticsPlaylist(a, playlist);
 
-  }
+        Date date = new Date();
+        timestamp = new Timestamp(date.getTime());
+        a = new Analytics(timestamp);
 
-  @Test
-  @DisplayName("playSong() WORKS")
-  void playSongGood() {
-    when(userRepo.findByUserId(any(Long.class))).thenReturn(user);
-    when(songRepo.findBySongId(any(Long.class))).thenReturn(song);
-    when(playlistRepo.findByPlaylistId(any(Long.class))).thenReturn(playlist);
-    when(analyticsRepo.save(any(Analytics.class))).thenReturn(a);
-    when(analyticsUserRepo.save(any(AnalyticsUser.class))).thenReturn(analyticsUser);
-    when(analyticsSongRepo.save(any(AnalyticsSong.class))).thenReturn(analyticsSong);
-    when(analyticsPlaylistRepo.save(any(AnalyticsPlaylist.class))).thenReturn(analyticsPlaylist);
+    }
 
-    Analytics aReturn = client1Service.playSong(1L, 1L, 1L);
-    assertNotNull(aReturn);
-    assertThat(aReturn.getTimestamp()).isEqualTo(timestamp);
-  }
+    @Test
+    @DisplayName("playSong() WORKS")
+    void playSongGood() {
+        when(userRepo.findByUserId(any(Long.class))).thenReturn(user);
+        when(songRepo.findBySongId(any(Long.class))).thenReturn(song);
+        when(playlistRepo.findByPlaylistId(any(Long.class))).thenReturn(playlist);
+        when(analyticsRepo.save(any(Analytics.class))).thenReturn(a);
+        when(analyticsUserRepo.save(any(AnalyticsUser.class))).thenReturn(analyticsUser);
+        when(analyticsSongRepo.save(any(AnalyticsSong.class))).thenReturn(analyticsSong);
+        when(analyticsPlaylistRepo.save(any(AnalyticsPlaylist.class))).thenReturn(analyticsPlaylist);
 
-  @Test
-  @DisplayName("playSong() FAILS, as expected")
-  void playSongBad() {
-    Analytics aReturn = client1Service.playSong(1L, 1L, 1L);
-    assertNull(aReturn);
-  }
+        Analytics aReturn = client1Service.playSong(1L, 1L, 1L);
+        assertNotNull(aReturn);
+        assertThat(aReturn.getTimestamp()).isEqualTo(timestamp);
+    }
+
+    @Test
+    @DisplayName("playSong() FAILS, as expected")
+    void playSongBad() {
+        Analytics aReturn = client1Service.playSong(1L, 1L, 1L);
+        assertNull(aReturn);
+    }
+
+    @Test
+    @DisplayName("likeSong() WORKS")
+    void likeSongGood() throws IllegalAccessException {
+        when(songRepo.findBySongId(any(Long.class))).thenReturn(song);
+
+        Integer originalSongLikesCount = song.getSongLikesCount();
+        for (int i = 0; i < 9; i++) {
+            client1Service.likeSong(1L, 1L);
+        }
+        Integer songLikesCount = client1Service.likeSong(1L, 1L);
+
+        assertEquals(originalSongLikesCount + 10, songLikesCount);
+    }
+
 
 }
