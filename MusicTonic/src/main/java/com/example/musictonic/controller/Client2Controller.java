9ca2ce09b9304750fe1ee8c035cafb9bac69d633
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-11-21 21:55:57 -0500

    fix CORS lol LOL LOL

--- MusicTonic/src/main/java/com/example/musictonic/controller/Client2Controller.java
+++ MusicTonic/src/main/java/com/example/musictonic/controller/Client2Controller.java
@@ -6,6 +6,7 @@ import com.example.musictonic.utils.PopularSongsReturn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Request handler for client type 2 (e.g., aggregated data metrics).
  */
+@CrossOrigin
 @RestController
 @RequestMapping("client2-rest")
 public class Client2Controller {
--- MusicTonic/src/main/java/com/example/musictonic/controller/Client3Controller.java
+++ MusicTonic/src/main/java/com/example/musictonic/controller/Client3Controller.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Request handler for client type 3 (e.g., raw data dump).
  */
+@CrossOrigin
 @RestController
 @RequestMapping("client3-rest")
 public class Client3Controller {
