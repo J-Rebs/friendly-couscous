ed93e3a388b9ba0be5c486051aa9c4dc373c4a43
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-11-24 17:34:37 -0500

    fix cors to allow delete mappings

--- MusicTonic/src/main/java/config/WebConfig.java
+++ MusicTonic/src/main/java/config/WebConfig.java
@@ -19,6 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
    */
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("/**");
+    registry.addMapping("/**").allowedOrigins("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
   }
 }
