7cda9a2204f6c2330374abc83f91a151b2e65789
Yuhao Dong <73535417+yuhao-dong@users.noreply.github.com>
2022-11-27 17:34:25 -0500

    testing CI

---
+++ MusicTonic/src/test/java/com/example/musictonic/controller/IntegrationTest.java
@@ -0,0 +1,53 @@
+package com.example.musictonic.controller;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationTest {
+  @Autowired
+  private TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+
+  @Test
+  public void getUsers() {
+    String url = "https://music-tonic.herokuapp.com/client1-rest/listUsers";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).build();
+    // if we need input parameter
+    // UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("param", "key").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.GET, req, String.class);
+
+    System.out.println(response.getBody());
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  public void getUsers_testing() {
+    String url = "https://music-tonic.herokuapp.com/client1-rest/listUsers";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).build();
+    // if we need input parameter
+    // UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("param", "key").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.GET, req, String.class);
+
+    System.out.println(response.getBody());
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+}
