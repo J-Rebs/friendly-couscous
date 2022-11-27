471c12d5ac70e18d8ce9c5e17a7f690e739252bc
Yuhao Dong <73535417+yuhao-dong@users.noreply.github.com>
2022-11-27 09:14:00 -0500

    Added Integration Test

---
+++ MusicTonic/src/test/java/com/example/musictonic/integration/IntegrationTest.java
@@ -0,0 +1,34 @@
+package com.example.musictonic.integration;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationTest {
+  @Autowired
+  private TestRestTemplate testRestTemplate;
+
+  @Test
+  public void getUsers() {
+    String url = "https://music-tonic.herokuapp.com/client1-rest/listUsers";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).build();
+    // if we need input parameter
+    // UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).queryParam("param", "key").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+    ResponseEntity<String> response = testRestTemplate.exchange(builder.toString(), HttpMethod.GET, req, String.class);
+
+    System.out.println(response.getBody());
+
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+}
