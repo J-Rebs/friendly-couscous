4c038b9f9718525a9eb87040ca50ab1cfd17b28f
Yuhao Dong <73535417+yuhao-dong@users.noreply.github.com>
2022-11-28 15:26:58 -0500

    added more tests

--- MusicTonic/src/test/java/com/example/musictonic/integration/IntegrationTest.java
+++ MusicTonic/src/test/java/com/example/musictonic/integration/IntegrationTest.java
@@ -32,7 +32,123 @@ public class IntegrationTest {
         testRestTemplate.exchange(builder.toString(), HttpMethod.GET, req, String.class);
 
     System.out.println(response.getBody());
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
 
+  @Test
+  public void playSong() {
+    String url = "https://music-tonic.herokuapp.com/client1-rest/playsong";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
+        .queryParam("userid", "1")
+        .queryParam("songid", "1")
+        .queryParam("playlistid", "1")
+        .queryParam("clientid", "1").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.POST, req, String.class);
+
+    System.out.println(response.getBody());
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+  }
+
+  @Test
+  public void likeSong() {
+    String url = "https://music-tonic.herokuapp.com/client1-rest/likeSong";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
+        .queryParam("userid", "1")
+        .queryParam("songid", "1")
+        .queryParam("clientid", "1").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.PUT, req, String.class);
+
+    System.out.println(response.getBody());
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  public void createUsers() {
+    String url = "https://music-tonic.herokuapp.com/client1-rest/createUser";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
+        .queryParam("realname", "Apple")
+        .queryParam("usertype", "LISTENER")
+        .queryParam("maingenre", "pop")
+        .queryParam("age", "16")
+        .queryParam("clientid", "1").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.POST, req, String.class);
+
+    System.out.println(response.getBody());
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+  }
+
+  @Test
+  public void deleteUsers() {
+    String url = "https://music-tonic.herokuapp.com/client1-rest/deleteUser";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
+        .queryParam("id", "11")
+        .queryParam("clientId", "1").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.DELETE, req, String.class);
+
+    System.out.println(response.getBody());
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+
+  // client 2
+  @Test
+  public void top3Songs() {
+    String url = "https://music-tonic.herokuapp.com/client2-rest/top3songs";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.GET, req, String.class);
+
+    System.out.println(response.getBody());
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  // Client 3
+  @Test
+  public void exportUsers() {
+    String url = "https://music-tonic.herokuapp.com/client3-rest/userexport";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
+        .queryParam("userid", "1")
+        .queryParam("clientid", "1").build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.GET, req, String.class);
+
+    System.out.println(response.getBody());
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  public void listSongs() {
+    String url = "https://music-tonic.herokuapp.com/client3-rest/listSongs";
+
+    UriComponents builder = UriComponentsBuilder.fromHttpUrl(url).build();
+    HttpEntity<String> req = new HttpEntity<>(null, null);
+
+    ResponseEntity<String> response =
+        testRestTemplate.exchange(builder.toString(), HttpMethod.GET, req, String.class);
+
+    System.out.println(response.getBody());
     assertEquals(HttpStatus.OK, response.getStatusCode());
   }
 }
