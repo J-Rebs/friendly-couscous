c44f3bb74179d232d682cbd528ef25ff68891cd8
Yuhao Dong <73535417+yuhao-dong@users.noreply.github.com>
2022-11-28 12:34:26 -0500

    testing CI

--- MusicTonic/src/test/java/com/example/musictonic/integration/IntegrationTest.java
+++ MusicTonic/src/test/java/com/example/musictonic/integration/IntegrationTest.java
@@ -1,6 +1,6 @@
 package com.example.musictonic.integration;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
