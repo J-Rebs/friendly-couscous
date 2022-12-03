c90851bf59aaa5e69521005bc80b201a531306b0
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-12-03 14:39:08 -0500

    git rid of deprecation

--- MusicTonic/src/main/java/com/example/musictonic/jwt/WebSecurityConfig.java
+++ MusicTonic/src/main/java/com/example/musictonic/jwt/WebSecurityConfig.java
@@ -4,25 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
   @Autowired
   private JwtTokenProvider jwtTokenProvider;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
 
     // Disable CSRF (cross site request forgery)
     http.csrf().disable();
@@ -46,33 +47,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // Optional, if you want to test the API from a browser
     // http.httpBasic();
+
+    return http.build();
   }
 
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    // Allow swagger to be accessed without authentication
-    // web.ignoring().antMatchers("/v2/api-docs")//
-    //  .antMatchers("/swagger-resources/**")//
-    //.antMatchers("/swagger-ui.html")//
-    //.antMatchers("/configuration/**")//
-    //.antMatchers("/webjars/**")//
-    //.antMatchers("/public")
-
-    // Un-secure H2 Database (for testing purposes, H2 console shouldn't be unprotected in production)
-    //.and()
-    //.ignoring()
-    //.antMatchers("/h2-console/**/**");;
-  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(12);
   }
 
-  @Override
   @Bean
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
-
 }
