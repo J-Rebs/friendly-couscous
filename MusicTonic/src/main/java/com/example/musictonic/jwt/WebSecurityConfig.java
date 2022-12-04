a49658d5fb072a20dca9e81b79521fc1cd2bdc8c
Ryan Soeyadi <rs4163@columbia.edu>
2022-12-04 01:59:58 -0500

    configured cors so authenticated http requests work from client

--- MusicTonic/src/main/java/com/example/musictonic/jwt/WebSecurityConfig.java
+++ MusicTonic/src/main/java/com/example/musictonic/jwt/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.musictonic.jwt;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Further web configurations to allow JWT authentication.
@@ -34,6 +40,8 @@ public class WebSecurityConfig {
 
     // Disable CSRF (cross site request forgery)
     http.csrf().disable();
+
+    http.cors().configurationSource(corsConfigurationSource());
 
     // No session will be created or used by spring security
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -68,5 +76,16 @@ public class WebSecurityConfig {
   public AuthenticationManager authenticationManager(
       AuthenticationConfiguration authenticationConfiguration) throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    configuration.setAllowedHeaders(Collections.singletonList("*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
